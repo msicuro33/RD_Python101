08cc7b4dd3d9d0576a61880b337836fa1e645524
Mattia <mattia470@hotmail.com>
2019-09-23 22:45:32 -0400

    Created startup.py file and adjusted userSetup file
    - Created startup.py file to test bringing in custom module when Maya opens (from userSetup.py file)
    - Added append and evalDeferred commands to userSetup file to import startup.py file

--- userSetup.py
+++ userSetup.py
@@ -1,1 +1,9 @@
-print("In Rig Tool")
+import os
+import sys
+import maya.cmds as cmds
+
+
+print("In User Setup")
+
+sys.path.append("C:/Users/MATTI/Documents/GitHub/RD_Python101")
+cmds.evalDeferred('import startup')
