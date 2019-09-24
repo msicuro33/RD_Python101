11fe5fe3d4d04e10e2740be3e0cbdc62da599e59
Mattia <mattia470@hotmail.com>
2019-09-24 00:08:02 -0400

    Attempt at importing ui folder and file from startup.py

--- startup.py
+++ startup.py
@@ -1,1 +1,14 @@
+import maya.cmds as cmds
+
 print("Startup")
+
+#Can set up custom settings
+#Use cmds.currentUnit to choose desired unit on startup
+
+#Change the current time unit to ntsc
+cmds.currentUnit(time = 'ntsc')
+
+#Change the currentlinear unit to centimeters
+cmds.currentUnit(linear = 'cm')
+
+import py101ui.rigui as ui
