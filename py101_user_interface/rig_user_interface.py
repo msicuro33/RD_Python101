c38ec14bbaba4433db4227b0f7b84f764b9ece63
Mattia <mattia470@hotmail.com>
2019-09-25 21:54:14 -0400

    Small change to ui file

--- py101_user_interface/rig_user_interface.py
+++ py101_user_interface/rig_user_interface.py
@@ -1,1 +1,10 @@
+import maya.cmds as cmds
+
 print('UI')
+
+def rigarm(*args):
+	print("Rig Arm is cool")
+
+mymenu = cmds.menu('RDojo_Menu', label = 'RD Menu', to = True, p = 'MayaWindow')
+cmds.menuItem(label = 'Rig_Arm', p = mymenu, command = rigarm)
+
