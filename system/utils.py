37df96288a143de7d1b276fe5a2e5f1af182b9e7
Mattia <mattia470@hotmail.com>
2019-10-08 19:31:33 -0400

    Added Read and Write json funtions in utils.py file

--- system/utils.py
+++ system/utils.py
@@ -2,8 +2,12 @@ import maya.cmds as cmds
 import json
 import tempfile
 
-def writeJson():
-	pass
+def writeJson(fileName,data):
+	with open(fileName, 'w') as outfile:
+		json.dump(data, outfile)
+	file.close(oufile)
 
-def readJson():
-	pass
+def readJson(fileName):
+	with open(fileName, 'r') as outfile:
+		data = (open(fileName, 'r').read())
+	return data
