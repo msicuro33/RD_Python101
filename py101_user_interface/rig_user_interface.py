ea16ea15f2581ee82fa5578b86c799252ce3e281
Mattia <mattia470@hotmail.com>
2019-10-28 22:00:03 -0400

    Started on UI class

--- py101_user_interface/rig_user_interface.py
+++ py101_user_interface/rig_user_interface.py
@@ -10,7 +10,24 @@ def rigarm(*args):
 	rig_arm = rig_arm.Rig_Arm()
 	print(rig_arm)
 	rig_arm.rig_arm()
+
+#The UI Class
+class RDojo_UI(object):
+	"""docstring for RDojo_UI"""
+	def __init__(self, *args):
+		print("In RDojo_UI")
+		mi = cmds.window("MayaWindow", ma = True, q = True)
+		for m in mi:
+			if m == "RDojo_Menu":
+				cmds.deleteUI('RDojo_Menu' ,m=True)
+
+		mymenu = cmds.menu('RDojo_Menu', label = 'RD Menu', to = True, p = 'MayaWindow')
+		cmds.menuItem(label = 'Rig_Tool', p = mymenu, command = self.ui)
+
+		#Create a dictionary to store UI elements we can access later
+		self.UIElements = {}
+
+
+ 		
 	
-mymenu = cmds.menu('RDojo_Menu', label = 'RD Menu', to = True, p = 'MayaWindow')
-cmds.menuItem(label = 'Rig_Arm', p = mymenu, command = rigarm)
 
