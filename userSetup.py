77155d693c5f0bf3d0e993bfb038f79ff8bdbfa7
Mattia <mattia470@hotmail.com>
2020-01-24 15:51:28 -0500

    Add note about module for User Setup file
    Added a note mentioning where the module that appends the path to the Python101 scripts folder is to show how the User Setup file starts up in Maya

--- userSetup.py
+++ userSetup.py
@@ -2,6 +2,12 @@ import os
 import sys
 import maya.cmds as cmds
 
+#References the RiggingTool.mod Module file in Maya Modules folder, looks like this:
+'''
+Adding new module = "+ Rigging_Tool 1.0 C:\Users\MATTI\Documents\GitHub\RD_Python101
+Create Environment variable = "RIGGING_TOOL=C:\Users\MATTI\Documents\GitHub\RD_Python101"
+Append to Mayas default scripts path = scripts: "C:\Users\MATTI\Documents\GitHub\RD_Python101"
+'''
 
 print("In User Setup")
 
