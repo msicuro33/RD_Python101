7a7755ee6dacb94cb92c7fb3b08cf7645caf624b
Mattia <mattia470@hotmail.com>
2020-02-04 22:51:02 -0500

    Made first pass of changes to make rig_arm more modular
    - Added self.rig _info to overwrite info from self.module_info
    - Moved other functions to utils.py file
    - Adjusted the arm.json file to allow extra convention for Left or Right sides

--- First_auto_rig/rig_arm.py
+++ First_auto_rig/rig_arm.py
@@ -2,6 +2,13 @@ import maya.cmds as cmds
 import json
 import os
 import system.utils as utils
+reload(utils)
+
+#Create variables above the Class level that can be read on Class import
+#This is also known as Attributes of a Class
+class_name = 'Rig_Arm'
+layout_file = arm.json
+num_joints = 4
 
 
 class Rig_Arm:
@@ -9,30 +16,53 @@ class Rig_Arm:
 	
 	def __init__(self):
 		#Get the joint list from the arm json file
-		data_path = os.environ["RDOJO_DATA"] + "/rig/arm.json"
+		data_path = os.environ["RDOJO_DATA"] + "data/rig/arm.json"
 		#Use the read json function
 		data = utils.readJson(data_path)
 		#Load the json into a dictionary
 		self.module_info = json.loads(data)
 		'''NOTE: If we want to build the arm from some set of joints
 		in the scene, we could overwrite self.module_info['positions']'''
+		
+		#Make new Dictionary to store information about the arm rig
+		self.rig_info = {}
+
+		#Check if we have a selection of joints to get new positions from
+		if len(cmds.ls(sl = True, type = 'joint',)) == num_joints:
+			sel = cmds.ls(sl = True, type = 'joint')
+			positions = []
+			for i in sel:
+				positions.append(cmds.xform(i, q = True, ws = True, t = True))
+			self.rig_info['positions'] = positions
+		else:
+			self.rig_info['positions'] = self.module_info['positions']
+		
+		'''Instead of using Else, we could just return a message saying the selection
+		doesn't meet the requirements for an arm'''
+
+		#Set a temporary variable to override the name of the side to determine Left or Right
+		self.instance = "L_"
+
+		#Run rig_arm function
+		self.rig_arm()
 
 
 	def rig_arm(self):
+		cmds.select(cl=True)
 		#################
 		##Create joints##
 		#################
 
 		#Create IK joints
-		self.createJoint(self.module_info['ik_joints'])
+		self.rig_info['ik_joints'] = utils.createJoint(self.module_info['ik_joints'], self.rig_info['positions'], self.instance)
 		cmds.select(cl=True)
 
 		#Create FK joints
-		self.createJoint(self.module_info['fk_joints'])
+		self.rig_info['fk_joints'] =utils.createJoint(self.module_info['fk_joints'])
 		cmds.select(cl=True)
 
 		#Create rig joints
-		self.createJoint(self.module_info['rig_joints'])
+		self.rig_info['rig_joints'] =utils.createJoint(self.module_info['rig_joints'])
 		cmds.select(cl=True)
 
 
@@ -41,10 +71,11 @@ class Rig_Arm:
 		#################
 
 		#1st Step: Create IK Handle
-		ik_handle = cmds.ikHandle(n=self.module_info['ik_controls'][1], sj=self.module_info['ik_joints'][0], ee=self.module_info['ik_joints'][2], sol='ikRPsolver',p = 2, w = 1)
+		ikHandle_name = self.module_info['ik_controls'][1].replace('s_',self.instance)
+		self.rig_info['ik_handle'] = cmds.ikHandle(n=ikHandle_name, sj=self.rig_info['ik_joints'][0], ee=self.rig_info['ik_joints'][2], sol='ikRPsolver',p = 2, w = 1)
 
 		#2nd Step: Define info to be passed into createControl function to create IK control
-		self.createControl([[self.module_info['positions'][2], self.module_info['ik_controls'][0]]])
+		self.rig_info['ik_controls'] = utils.createControl([[self.rig_info['positions'][2], self.module_info['ik_controls'][0].replace('s_',self.instance)]])
 		#Thought it should be: self.createControl([[self.module_info['positions'][2], self.module_info['ik_controls'][0], self.module_info['ik_controls'][1]]])
 		#But there isn't a name in the data for an IK ctrlgroup name
 
@@ -95,53 +126,7 @@ class Rig_Arm:
 		#To switch between FK and IK, change values of the two attributes (FK shoulder joint and IK shoulder joint)
 		# and set one or the other to zero		
 
-	def createJoint(self, joint_info):
-		'''
-		Takes in joint info as an argument and iterates through the name and position to create joint
-		'''
-		[cmds.joint(n=joint_info[i], p=self.module_info['positions'][i]) for i in range(len(joint_info))]
-
-
-	def createControl(self, ctrlInfo):
-		'''
-		Iterates through joint_names+positions to create control curves
-		'''
-		control_info = []
-		for info in ctrlInfo:
-			#Get WS position of joint
-			pos = info[0]
-			#Create an empty group
-			ctrl_group = cmds.group(em=1, n="group_" + info[1])
-			#Create circle control object
-			ctrl = cmds.circle(n=info[1])
-			#Parent the control under the group
-			cmds.parent(ctrl,ctrl_group)
-			#Move the group to the joint
-			cmds.xform(ctrl_group, t = pos, ws = True)
-			#Append control info to control_info List
-			control_info.append([ctrl_group, ctrl])
-		return(control_info)
-
-	def calculatePoleVectorPosition(self, joints):
-		from maya import cmds , OpenMaya
-		start = cmds.xform(joints[0], q = True, ws = True, t = True)
-		mid = cmds.xform(joints[1], q = True, ws = True, t = True)
-		end = cmds.xform(joints[2], q = True, ws = True, t = True)
-		
-		startVector = OpenMaya.MVector(start[0], start[1], start[2])
-		midVector = OpenMaya.MVector(mid[0], mid[1], mid[2])
-		endVector = OpenMaya.MVector(end[0], end[1], end[2])
-		
-		startEnd = endVector - startVector
-		startMid = midVector - startVector
-		dotP = startMid * startEnd
-		proj = float(dotP) / float(startEnd.length())
-		startEndN = startEnd.normal()
-		projV = startEndN * proj
-		arrowV = startMid - projV
-		arrowV *= 0.5
-		finalV = arrowV + midVector
-		return([finalV.x, finalV.y, finalV.z])
+	
 
 
 
--- system/utils.py
+++ system/utils.py
@@ -11,3 +11,54 @@ def readJson(fileName):
 	with open(fileName, 'r') as outfile:
 		data = (open(fileName, 'r').read())
 	return data
+
+
+def createJoint(name, position, instance):
+	'''
+	Takes in joint info as an argument and iterates through the name and position to create joint
+	'''
+	joint_list = [cmds.joint(n=name[i].replace('s_', instance), p=position[i]) for i in range(len(name))]
+	cmds.select(cl=True)
+	return(joint_list)
+
+
+def createControl(ctrlInfo):
+	'''
+	Iterates through joint_names+positions to create control curves
+	'''
+	control_info = []
+	for info in ctrlInfo:
+		#Get WS position of joint
+		pos = info[0]
+		#Create an empty group
+		ctrl_group = cmds.group(em=1, n="group_" + info[1])
+		#Create circle control object
+		ctrl = cmds.circle(n=info[1])
+		#Parent the control under the group
+		cmds.parent(ctrl,ctrl_group)
+		#Move the group to the joint
+		cmds.xform(ctrl_group, t = pos, ws = True)
+		#Append control info to control_info List
+		control_info.append([ctrl_group, ctrl])
+	return(control_info)
+
+def calculatePoleVectorPosition(joints):
+	from maya import cmds , OpenMaya
+	start = cmds.xform(joints[0], q = True, ws = True, t = True)
+	mid = cmds.xform(joints[1], q = True, ws = True, t = True)
+	end = cmds.xform(joints[2], q = True, ws = True, t = True)
+	
+	startVector = OpenMaya.MVector(start[0], start[1], start[2])
+	midVector = OpenMaya.MVector(mid[0], mid[1], mid[2])
+	endVector = OpenMaya.MVector(end[0], end[1], end[2])
+	
+	startEnd = endVector - startVector
+	startMid = midVector - startVector
+	dotP = startMid * startEnd
+	proj = float(dotP) / float(startEnd.length())
+	startEndN = startEnd.normal()
+	projV = startEndN * proj
+	arrowV = startMid - projV
+	arrowV *= 0.5
+	finalV = arrowV + midVector
+	return([finalV.x, finalV.y, finalV.z])
